db20c15f49edbd44ede287f48a8e09e99efde003
it5pa <sendingthistopaula@gmail.com>
2022-08-28 20:05:11 +0300

    all of it

---
+++ script.js
@@ -0,0 +1,50 @@
+$(function() {
+
+    new Slider({
+        images: '.slider-1 img',
+        btnPrev: '.slider-1 .buttons .prev',
+        btnNext: '.slider-1 .buttons .next',
+        auto: false
+    });
+});
+
+
+function Slider(obj) {
+
+  this.images = $(obj.images);
+  this.auto = obj.auto;
+  this.btnPrev = obj.btnPrev;
+  this.btnNext = obj.btnNext;
+    this.rate = obj.rate || 1000;
+
+  var i = 0;
+    var slider = this;
+
+    // The "Previous" button: to remove the class .shoved, show the previous image and add the .shoved class
+  this.prev = function () {
+    slider.images.eq(i).removeClass('shown');
+    i--;
+
+    if (i < 0) {
+      i = slider.images.length - 1;
+    }
+
+    slider.images.eq(i).addClass('shown');
+  }
+
+    // The "Next" button: to remove the class .shoved, show the next image and add the .shoved class
+  this.next = function () {
+    slider.images.eq(i).removeClass('shown');
+    i++;
+
+    if (i >= slider.images.length) {
+      i = 0;
+    }
+
+    slider.images.eq(i).addClass('shown');
+  }
+
+    // To add next and prev functions when clicking the corresponding buttons
+    $(slider.btnPrev).on('click', function(){ slider.prev();});
+    $(slider.btnNext).on('click', function(){ slider.next();});
+};
