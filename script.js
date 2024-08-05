db12a5207799ec6312956e0e56fe7d3bcd4cce80
it5pa <34326465+it5pa@users.noreply.github.com>
2024-08-05 15:56:07 +0200

    Whole new web

--- script.js
+++ script.js
@@ -1,86 +1,114 @@
-const listen = document.querySelector(".listen");
-const mixes = document.querySelector(".mixes");
-
-listen.addEventListener('mouseover', ()=>{
-  mixes.style ="display:block";
-})
-
-mixes.addEventListener('mouseover', ()=>{
-  mixes.style ="display:block";
-})
-
-mixes.addEventListener('mouseout', ()=>{
-  mixes.style ="display:none";
-})
-
-
-if (window.innerWidth < 960) {
-  listen.addEventListener('click', ()=> {
-    if (mixes.style.display == "none") {
-      mixes.style.display = "block";
-    } else {
-      mixes.style.display = "none";
-    }
-  })
-}
-
-$(function() {
-
-    new Slider({
-        images: '.slider-1 img',
-        btnPrev: '.slider-1 .buttons .prev',
-        btnNext: '.slider-1 .buttons .next',
-        auto: false
+$(document).ready(function(){
+    $('.project').slick({
+      dots: false,
+      respondTo: 'slider',
+      draggable: true,
+      infinite: true,
+      slidesToShow: 1,
+      speed: 150,
+      fade: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 620,
+          settings: {
+          arrows: false,
+          adaptiveHeight: true
+          }
+        },
+        {
+          breakpoint: 345,
+          settings: {
+          arrows: false,
+          adaptiveHeight: true
+          }
+        }
+        ]
     });
-});
+  });
 
+  
 
-function Slider(obj) {
+  const worksButton = document.querySelector(".works");
 
-  this.images = $(obj.images);
-  this.auto = obj.auto;
-  this.btnPrev = obj.btnPrev;
-  this.btnNext = obj.btnNext;
-    this.rate = obj.rate || 1000;
+  worksButton.addEventListener("click", worksToggle);
 
-  var i = 0;
-    var slider = this;
+  var moreInfo = document.getElementById("more-info");
+  var projects = document.getElementById("projects");
+  var mobileView = window.matchMedia("(max-width: 640px)")
+  const infoButton = document.querySelector(".info");
 
-    // The "Previous" button: to remove the class .showed, show the previous image and add the .showed class
-  this.prev = function () {
-    slider.images.eq(i).removeClass('shown');
-    i--;
+  if (mobileView.matches) { 
+    worksButton.innerHTML = "Works & More";
+    worksButton.style.width = "unset";
+    infoButton.style.display = "none";
+    worksButton.style.color = "#EC825B";
+  };
 
-    if (i < 0) {
-      i = slider.images.length - 1;
+  function worksToggle() {
+    var projects = document.getElementById("projects");
+
+    if (projects.style.opacity == "0") {
+      projects.style.opacity = "1";
+      projects.style.display = "block"
+      projects.style.pointerEvents = "unset";
+      worksButton.style.color = "#DCDCDCDC";
+    } else {
+      projects.style.pointerEvents = "none";
+      projects.style.display = "none"
+      projects.style.opacity = "0";
+      worksButton.style.color = "#EC825B";
     }
 
-    slider.images.eq(i).addClass('shown');
-  }
+    var mobileView = window.matchMedia("(max-width: 640px)")
 
-    // The "Next" button: to remove the class .showed, show the next image and add the .showed class
-  this.next = function () {
-    slider.images.eq(i).removeClass('shown');
-    i++;
+    function mobileProjectsToggle(mobileView) {
+      worksButton.style.color = "#DCDCDCDC";
+      if (mobileView.matches) { // If media query matches
+        if (projects.style.display == "block") {
+          moreInfo.style.display = "none";
+          worksButton.style.color = "#EC825B";
+        } else if (projects.style.display == "none") {
+          moreInfo.style.display = "block";
+        } 
+      } 
+    }
+  
+    mobileProjectsToggle(mobileView);
+  };
 
-    if (i >= slider.images.length) {
-      i = 0;
+
+  infoButton.addEventListener("click", infoToggle);
+
+  function infoToggle() {
+    var moreInfo = document.getElementById("more-info");
+    if (moreInfo.style.opacity == "1") {
+      moreInfo.style.opacity = "0";
+      moreInfo.style.display = "none"
+      moreInfo.style.pointerEvents = "none";
+      infoButton.style.color = "#EC825B";
+    } else {
+      moreInfo.style.opacity = "1";
+      moreInfo.style.display = "block"
+      moreInfo.style.pointerEvents = "unset";
+      infoButton.style.color = "#DCDCDCDC";
     }
 
-    slider.images.eq(i).addClass('shown');
-  }
+    var mobileView = window.matchMedia("(max-width: 640px)")
 
-    // To add next and prev functions when clicking the corresponding buttons
-    $(slider.btnPrev).on('click', function(){ slider.prev();});
-    $(slider.btnNext).on('click', function(){ slider.next();});
+    function mobileInfoToggle(mobileView) {
+      if (mobileView.matches) { // If media query matches
+        var moreInfo = document.getElementById("more-info");
+        if (projects.style.display == "block") {
+          moreInfo.style.display = "block";
+          projects.style.display = "none";
+        } else {
+          moreInfo.style.display = "block";
+        }
+      }
+    }
+  
+    mobileInfoToggle(mobileView);
+  };
 
-    document.addEventListener("keydown", event => {
-  //every time a key is pushed, this function will fire
-  event.code === "ArrowLeft" && slider.prev();
-  event.code === "ArrowRight" && slider.next();
-});
-
-$("#right-arrow").click(function() { slider.next();});
-$("#left-arrow").click(function() {slider.prev();});
-
-};
+  
